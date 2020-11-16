b7f064a38494259a806aecfa3bd424872bee1f39
Hannarong Klinjan <hannarong98@hotmail.com>
2020-11-16 17:09:04 +0100

    Add script to push image after_success

---
+++ scripts/pushToRegistry.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+   set -ex;
+    docker build -t "${IMAGE}:${TRAVIS_COMMIT}" . && \
+    docker push "${IMAGE}:${TRAVIS_COMMIT}"
+    set +x
+fi
