51f6e8051b3c832d9df2e72ef2cc15504770b95e
Hannarong Klinjan <hannarong98@hotmail.com>
2020-11-21 06:47:29 +0100

    Fix test configuration

--- src/test/kotlin/no/kristiania/pgr301/exam/RestApiTest.kt
+++ src/test/kotlin/no/kristiania/pgr301/exam/RestApiTest.kt
@@ -19,13 +19,13 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
-import org.springframework.context.annotation.Profile
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import javax.annotation.PostConstruct
 
 @ExtendWith(SpringExtension::class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Profile("test")
+@SpringBootTest(classes = [ Application::class ], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class RestApiTest{
 
     @LocalServerPort
